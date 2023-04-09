6ed1c3518894a9ddec6560476310f5da3d7ac5c4
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-09 22:55:55 +0930

    um what

--- QuickSort.h
+++ QuickSort.h
@@ -7,7 +7,8 @@
 class QuickSort:public Sort
 {
     public:
-    virtual std::vector<int> sort(std::vector<int> list);
+    // virtual sort
+    std::vector<int> sort(std::vector<int> list);
     std::vector<int> partitionfunction(std::vector<int> list, int smaller, int higher);
     std::vector<int> helperfunction(std::vector<int> list, int low, int high);
     void swap(int &a, int &b);
