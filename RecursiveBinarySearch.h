6e9b3346eca22aac07c8b880f5ef402b0c8895e2
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-10 15:27:15 +0930

    um wtf

--- RecursiveBinarySearch.h
+++ RecursiveBinarySearch.h
@@ -1,11 +1,12 @@
-#ifndef RECURSIVE_BINARY_SEARCH
+#ifndef RECURSIVE_BINARY_SEARCH_H
 #define RECURSIVE_BINARY_SEARCH
 #include <vector>
 
 class RecursiveBinarySearch
 {
     public:
-    bool search(std::vector<int> list, int target);
+    // bool search(std::vector<int> list, int target);
+    bool search(std::vector<int>, int);
     bool helperfunction(std::vector <int> list, int target, int start, int end);
 };
 
