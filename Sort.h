722e07c61ac34265d5341ca3f6cb75ff8779e119
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-04-10 15:37:14 +0930

    huh

--- Sort.h
+++ Sort.h
@@ -6,7 +6,10 @@
 class Sort 
 {
     public:
-    virtual std::vector<int> sort(std::vector<int> list) = 0;
+    virtual std::vector<int> sort(std::vector<int> list)
+    {
+        //Nothing
+    };
 };
 
 #endif
